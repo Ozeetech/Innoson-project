da6abc9bf4e2d0f8df7880af8c6ac9af38ebf64d
Ozee Tech <ozeetechgadgets@gmail.com>
2024-09-12 01:30:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './layouts/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import navLogo from "../assets/NavLogo.png";
+import "../style/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='container py-2 d-flex
+      justify-content-between'>
+        <div className='d-flex align-items-center gap-2'>
+          <img src={navLogo} alt="nav-logo" className='img-flui' />
+          <h2>Innoson Group</h2>
+        </div>
+        <ul className='d-flex align-items-center gap-5
+        list-unstyled'>
+          <li>Home</li>
+          <li>Companies</li>
+          <li>Products</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  ); 
+};
+
+export default Navbar;
